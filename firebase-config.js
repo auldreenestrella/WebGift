603d2c734e63fb5d21135e2657dd6cded7cbae94
AuldreenZhen <auldreenestrella@gmail.com>
2026-07-28 13:01:39 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCd3pzay8PdcnuYvTb5_HqRieKEeGonQG4",
+  authDomain: "webgift-72f45.firebaseapp.com",
+  projectId: "webgift-72f45",
+  storageBucket: "webgift-72f45.firebasestorage.app",
+  messagingSenderId: "228593510231",
+  appId: "1:228593510231:web:6824f88502ec6ba4e94012"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
